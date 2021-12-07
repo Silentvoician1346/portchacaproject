918b4d614bb9249df22c6becc427ee7a2d3790c0
Silentvoician1346 <silentvoician1346@gmail.com>
2021-12-07 20:43:44 +0700

    before start

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Code,
+  List,
+  ListIcon,
+  ListItem,
+  Container,
+} from "@chakra-ui/react";
+import { CheckCircleIcon, LinkIcon } from "@chakra-ui/icons";
+
+export default function Index() {
+  return (
+    <Container height="100vh" fontFamily="Spartan, sans-serif">
+      <Text>ABCDE</Text>
+      <Text>BASFASF</Text>
+    </Container>
+  );
+}
